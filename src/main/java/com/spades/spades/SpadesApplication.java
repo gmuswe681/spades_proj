96f7719a3883f7f5ad26ad29853bd94dbc9b305b
Jamie Bowers <jamiebrs@gmail.com>
2019-03-24 15:50:48 -0400

    Initial commit

---
+++ src/main/java/com/spades/spades/SpadesApplication.java
@@ -0,0 +1,13 @@
+package com.spades.spades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpadesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpadesApplication.class, args);
+    }
+
+}
