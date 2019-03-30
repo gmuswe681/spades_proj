a6b4f24d543e3bbdecf276b07ee7d7c332b31fbd
drkluong8247 <dmtluong@gmail.com>
2019-03-30 16:40:39 -0400

    Adds basic database entry for users.

--- src/main/java/com/spades/spades/resources/CreateUserController.java
+++ src/main/java/com/spades/spades/resources/CreateUserController.java
@@ -6,16 +6,37 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.spades.spades.model.Users;
+import com.spades.spades.repository.UsersRepository;
+
 @RequestMapping("/createuser")
 @RestController
 public class CreateUserController {
+
+    private final UsersRepository repository;
+
+    CreateUserController(UsersRepository repository) {
+        this.repository = repository;
+    }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
     public String submitUser(HttpServletRequest hReq) {
         String firstName = hReq.getParameter("firstname");
         String lastName = hReq.getParameter("lastname");
-        String userName = hReq.getParameter("user");
+        String email = hReq.getParameter("user");
         String password = hReq.getParameter("password");
-        return "user was entered: " + userName + ", " + password + ", Human name is " + firstName + " " + lastName;
+
+        if ((firstName == null) || (lastName == null) || (email == null) || (password == null))
+        {
+            return "A field wasn't specified";
+        }
+
+        Users newUser = new Users();
+        newUser.setName(firstName);
+        newUser.setLastName(lastName);
+        newUser.setEmail(email);
+        newUser.setPassword(password);
+        repository.save(newUser);
+        return "user was entered: " + firstName + ", " + password + ", Human name is " + firstName + " " + lastName;
     }
 }
