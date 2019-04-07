c9515b9d80123f4364fe442f7655709faeea2c02
Jamie Bowers <jamiebrs@gmail.com>
2019-04-07 15:45:04 -0400

    Fixed User_role to remove unique constraint.

--- sql_scripts/create_user_role.sql
+++ sql_scripts/create_user_role.sql
@@ -7,8 +7,6 @@ CREATE TABLE public.user_role
     user_id integer NOT NULL,
     role_id integer NOT NULL,
     CONSTRAINT pk_user_role PRIMARY KEY (user_id, role_id),
-    CONSTRAINT uk_it77eq964jhfqtu54081ebtio UNIQUE (role_id)
-,
     CONSTRAINT fk_role FOREIGN KEY (role_id)
         REFERENCES public.role (role_id) MATCH SIMPLE
         ON UPDATE NO ACTION
@@ -18,10 +16,10 @@ CREATE TABLE public.user_role
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
+    WITH (
+        OIDS = FALSE
+    )
+    TABLESPACE pg_default;
 
 ALTER TABLE public.user_role
     OWNER to postgres;
