f10e9b5a9ea80fdddcb8942a6195e003e23addca
Jamie Bowers <jamiebrs@gmail.com>
2019-04-13 16:33:21 -0400

    Put logging into the General Error Controller

--- src/main/java/com/spades/spades/resources/GeneralErrorController.java
+++ src/main/java/com/spades/spades/resources/GeneralErrorController.java
@@ -1,14 +1,23 @@
 package com.spades.spades.resources;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class GeneralErrorController implements ErrorController {
 
     @RequestMapping("/error")
-    public String errorResponse() {
+    public String errorResponse(HttpServletRequest request) {
+         final Logger LOGGER = LogManager.getLogger("GeneralErrorController.class");
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
+
+
         String result = "<html>\n";
         result += "<head></head>\n";
         result += "<body>\n";
@@ -16,7 +25,17 @@ public class GeneralErrorController implements ErrorController {
         result += "<a href=\"/\">Go to homepage</a>\n";
         result += "</body>\n";
         result += "</html>";
+
+        String error =  String.format("Status code: %s "
+                        + "Exception Message: %s",
+                statusCode, exception==null? "N/A": exception.getMessage());
+
+        LOGGER.error(error);
+
+
         return result;
+
+
     }
 
     @Override
