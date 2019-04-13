78cde922933ee9c520b0f679bdce36a4eca5735a
Jamie Bowers <jamiebrs@gmail.com>
2019-04-13 16:00:12 -0400

    Got Port forwarding to work so that port 80 the default port for http, forwards to port 843 the default port for https, and you dont have to put port numbers at the end of the URL.

--- src/main/java/com/spades/spades/SpadesApplication.java
+++ src/main/java/com/spades/spades/SpadesApplication.java
@@ -1,7 +1,14 @@
 package com.spades.spades;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class SpadesApplication {
@@ -10,4 +17,41 @@ public class SpadesApplication {
         SpringApplication.run(SpadesApplication.class, args);
     }
 
+    @Configuration
+    public class HttpsRedirectConf {
+        private final static String SECURITY_USER_CONSTRAINT = "CONFIDENTIAL";
+        private final static String REDIRECT_PATTERN = "/*";
+        private final static String CONNECTOR_PROTOCOL = "org.apache.coyote.http11.Http11NioProtocol";
+        private final static String CONNECTOR_SCHEME = "http";
+
+
+        @Bean
+        public TomcatServletWebServerFactory servletContainer() {
+            TomcatServletWebServerFactory tomcat =
+                    new TomcatServletWebServerFactory() {
+
+                        @Override
+                        protected void postProcessContext(Context context) {
+                            SecurityConstraint securityConstraint = new SecurityConstraint();
+                            securityConstraint.setUserConstraint(SECURITY_USER_CONSTRAINT);
+                            SecurityCollection collection = new SecurityCollection();
+                            collection.addPattern(REDIRECT_PATTERN);
+                            securityConstraint.addCollection(collection);
+                            context.addConstraint(securityConstraint);
+                        }
+                    };
+            tomcat.addAdditionalTomcatConnectors(createHttpConnector());
+            return tomcat;
+        }
+
+        private Connector createHttpConnector() {
+            Connector connector =
+                    new Connector(CONNECTOR_PROTOCOL);
+            connector.setScheme(CONNECTOR_SCHEME);
+            connector.setSecure(false);
+            connector.setPort(80);
+            connector.setRedirectPort(443);
+            return connector;
+        }
+    }
 }
