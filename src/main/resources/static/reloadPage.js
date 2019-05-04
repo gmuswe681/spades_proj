f7e8695f0b359b22daa6c9ac11428544609d9d88
Jamie Bowers <jamiebrs@gmail.com>
2019-05-04 15:58:59 -0400

    Fixed Javascript reload.

--- src/main/resources/static/reloadPage.js
+++ src/main/resources/static/reloadPage.js
@@ -6,23 +6,45 @@ document.addEventListener('DOMContentLoaded', function(){
     bidAmountelement = document.getElementById("bidAmount");
     cardElement = document.getElementById("card");
     if(bidAmountelement != null){
-        bidAmountelement.addEventListener("change", stopTimeout);
+        bidAmountelement.addEventListener("keydown", stopTimeout);
     }
     if(cardElement != null){
-        cardElement.addEventListener("change", stopTimeout);
+        cardElement.addEventListener("keydown", checkForValue);
+        //cardElement.addEventListener("focus", checkForValue);
     }
     startTimer();
 }, false)
 
 function startTimer() {
-    timeout = setTimeout(reloadPage, 5000);
+    timeout = setTimeout(reloadPage, 7000);
 }
 
 function reloadPage(){
+    console.log('reload method')
     location.reload();
 }
 
 
 function stopTimeout(){
+    console.log('stop timeout method')
  clearTimeout(timeout);
+}
+
+function checkForValue(){
+    console.log('check for value')
+    if(cardElement != null){
+        if(cardElement.value != ""){
+            stopTimeout();
+        }
+    }
+
+    if(bidAmountelement != null){
+        console.log('a')
+        if(bidAmountelement.value != ""){
+            console.log('b')
+            stopTimeout();
+        }
+    }
+
+
 }
